de2caa7e378b717d94dfe1e1010c36bf14bc61ec
RS181 <ruisantos.18181@gmail.com>
2023-12-02 13:41:16 +0000

    Mexi na organização do código

--- Servidor/index.js
+++ Servidor/index.js
@@ -41,87 +41,24 @@ http.createServer(function (request, response) {
     // response.write("URL = " + JSON.stringify(parsedURL.query) + "\n");
     // response.write("Path = " + pathname + "\n");
 
+    // Configuração do CORS para permitir qualquer origem
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    // Resposta para solicitações OPTIONS
+    if (request.method === 'OPTIONS') {
+        response.writeHead(200, headers.plain);
+        response.end();
+        return;
+    }
 
     //Dividimos o que fazer com cada pedido consoante o /
     console.log("=======Inicio Pedido=======")
     switch (pathname) {
         case '/register':
             console.log("Entrei no /register");
-
-            //obter dados do pedido (quando bloco de dados estiver disponivel)
-            let requestData = '';
-            request.on('data', chunk => {
-                requestData += chunk.toString();
-                console.log("Request Data = " + requestData);
-            });
-
-            //Quando a leitura terminar 
-            request.on('end', () => {
-                try {
-                    // Serialização dos dados no pedido
-                    const requestDataObj = JSON.parse(requestData);
-
-                    // Ler os dados do arquivo
-                    fs.readFile('dados.txt', 'utf8', (err, data) => {
-                        if (err) {
-                            console.error("Erro ao ler dados do arquivo:", err);
-                            response.writeHead(500, headers.plain); 
-                            response.end('{"error": "Internal Server Error"}');
-                            return;
-                        }
-
-                        let userDatabase = {};
-
-                        //Tentamos colocar os dados em dados.txt (no objeto userDatabase)
-                        if (data) {
-                            try {
-                                userDatabase = JSON.parse(data);
-                            } catch (parseError) {
-                                console.error("Erro ao analisar dados do arquivo JSON:", parseError);
-                                response.writeHead(500, headers.plain);
-                                response.end('{"error": "Internal Server Error"}');
-                                return;
-                            }
-                        }
-
-                        // Verificar se o utilizador já está registrado
-                        if (userDatabase[requestDataObj.nick]) {
-                            // Verificar se a password está correta
-                            if (userDatabase[requestDataObj.nick] === requestDataObj.password) {
-                                // Sucesso: utilizador registado e password correta
-                                response.writeHead(200, headers.plain);
-                                response.end("{}");
-                            } else {
-                                // password diferente
-                                response.writeHead(200, headers.plain);
-                                response.end('{"error": "User registered with a different password"}');
-                            }
-                        } else {
-                            // Utilizador não registrado(Adicionar)
-                            userDatabase[requestDataObj.nick] = requestDataObj.password;
-
-                            // Escrever os dados atualizados no arquivo
-                            fs.writeFile('dados.txt', JSON.stringify(userDatabase), 'utf8', (writeErr) => {
-                                if (writeErr) {
-                                    console.error("Erro ao escrever dados no arquivo:", writeErr);
-                                    response.writeHead(500, headers.plain); 
-                                    response.end('{"error": "Internal Server Error"}');
-                                    return;
-                                }
-
-                                // Responder com sucesso
-                                response.writeHead(200, headers.plain);
-                                response.end("{}");
-                            });
-                        }
-                    });
-                } catch (error) {
-                    // Erro ao analisar os dados do
-                    response.writeHead(400, headers.plain); 
-                    response.end('{"error": "Invalid request data"}');
-                }
-            });
-
+            register(request,response);
             break;
         case '/ranking':
             console.log("Entrei no /ranking");
@@ -145,3 +82,84 @@ http.createServer(function (request, response) {
 }).listen(PORT);
 
 
+
+
+function register(request, response) {
+
+    //obter dados do pedido (quando bloco de dados estiver disponivel)
+    let requestData = '';
+    request.on('data', chunk => {
+        requestData += chunk.toString();
+        console.log("Request Data = " + requestData);
+    });
+
+    //Quando a leitura terminar 
+    request.on('end', () => {
+        try {
+            // Serialização dos dados no pedido
+            const requestDataObj = JSON.parse(requestData);
+
+            // Ler os dados do arquivo
+            fs.readFile('dados.txt', 'utf8', (err, data) => {
+                if (err) {
+                    console.error("Erro ao ler dados do arquivo:", err);
+                    response.writeHead(500, headers.plain);
+                    response.end('{"error": "Internal Server Error"}');
+                    return;
+                }
+
+                let userDatabase = {};
+
+                //Tentamos colocar os dados em dados.txt (no objeto userDatabase)
+                if (data) {
+                    try {
+                        userDatabase = JSON.parse(data);
+                    } catch (parseError) {
+                        console.error("Erro ao analisar dados do arquivo JSON:", parseError);
+                        response.writeHead(500, headers.plain);
+                        response.end('{"error": "Internal Server Error"}');
+                        return;
+                    }
+                }
+
+                // Verificar se o utilizador já está registrado
+                if (userDatabase[requestDataObj.nick]) {
+                    // Verificar se a password está correta
+                    if (userDatabase[requestDataObj.nick] === requestDataObj.password) {
+                        // Sucesso: utilizador registado e password correta
+                        response.writeHead(200, headers.plain);
+                        response.end("{}");
+                        console.log("=======Fim Pedido=======");
+                    } else {
+                        // password diferente
+                        response.writeHead(200, headers.plain);
+                        response.end('{"error": "User registered with a different password"}');
+                        console.log("=======Fim Pedido=======");
+                    }
+                } else {
+                    // Utilizador não registrado(Adicionar)
+                    userDatabase[requestDataObj.nick] = requestDataObj.password;
+
+                    // Escrever os dados atualizados no arquivo
+                    fs.writeFile('dados.txt', JSON.stringify(userDatabase), 'utf8', (writeErr) => {
+                        if (writeErr) {
+                            console.error("Erro ao escrever dados no arquivo:", writeErr);
+                            response.writeHead(500, headers.plain);
+                            response.end('{"error": "Internal Server Error"}');
+                            return;
+                        }
+
+                        // Responder com sucesso
+                        response.writeHead(200, headers.plain);
+                        response.end("{}");
+                    });
+                }
+            });
+        } catch (error) {
+            // Erro ao analisar os dados do
+            response.writeHead(400, headers.plain);
+            response.end('{"error": "Invalid request data"}');
+        }
+    });
+
+}
