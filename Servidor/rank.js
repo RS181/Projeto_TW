b4f1f5a2287a7c8d7484edf6a9dc8ba8dd81b492
RS181 <ruisantos.18181@gmail.com>
2023-12-03 19:04:54 +0000

    Modulo rank.js e rankData.txt (guarda tabela de score)

---
+++ Servidor/rank.js
@@ -0,0 +1,63 @@
+// Trata de guardar scores 
+const { error } = require('console');
+const fs = require('fs');
+
+module.exports.Score = class {
+
+    Score(group, rows, columns) {
+        this.group = group;
+        this.rows = rows;
+        this.columns = columns;
+        this.rankData = {};
+        this.filePath = 'rankData.txt';
+    }
+
+    //Atualiza o score dos jogadores envolvido num jogo 
+    SaveScore(player1, player2, winner) {
+        //todo 
+    }
+
+    // Adiciona um utilizador ao score (deve ser feito caso utilizador novo seja registado)
+    AddUserToScore(name) {
+        console.log("=> " + name);
+    }
+
+    //Retorna em formato de string a tabela de resultado para um jogo 
+    GetScore() {
+        //todo
+    }
+
+    // Método  para salvar dados num ficheiro txt
+    saveToFile() {
+        //todo
+    }
+
+    // Método que carrega dados do ficheiro
+    loadFromFile(){
+        fs.readFile('rankData.txt','utf8',(err,data) => {
+            if (err){
+                console.log("Erro ao ler dados do arquivo:", err);
+                return null;
+            }
+
+            //objeto que guarda dados provenientes de .txt
+            let rankDatabase = {};
+            
+            if (data){
+                try{
+                    rankDatabase = JSON.parse(data);
+                }catch(parseError){
+                    console.log("Erro ao analisar dados do ficheiro " + this.filePath + ":", parseError);
+                    return null;
+                }
+            }
+
+            // console.log("=>" + JSON.stringify(rankDatabase));
+            console.log(rankDatabase["group_"+this.group]);
+            console.log(rankDatabase["group_"+this.group][this.rows + "_por_" + this.columns]);
+
+
+        })
+    }
+
+};
