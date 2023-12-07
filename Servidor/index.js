4ff2076a2224abfd461c24418196fe046f8ca063
RS181 <ruisantos.18181@gmail.com>
2023-12-07 21:31:22 +0000

    Correção do /register

--- Servidor/index.js
+++ Servidor/index.js
@@ -4,17 +4,21 @@
 "use strict";
 
 let PORT = 8008;
+let host = "localhost"
 
 //Modulos 
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const rank = require('./rank.js');
-const { Console } = require('console');
 
 //todo importar os modulos para restantes funções
 
+//Entrega do trabalho dia 15 !!
+//Teste dia 18 
 
+//todo professor sugeriu no inicio , carregar objetos 
+//todo no inicio que tem utilizadores e ranking
 
 
 /*  Cabeçalhos  */
@@ -33,8 +37,13 @@ const headers = {
 };
 
 
+//todo tentar aplicar isto https://www.knowledgehut.com/blog/web-development/node-http-server
+
+
+
+
 /* Servidor */
-http.createServer(function (request, response) {
+const Server = http.createServer(function (request, response) {
     const parsedURL = url.parse(request.url, true);
     const pathname = parsedURL.pathname;
     const query = JSON.stringify(parsedURL.query);
@@ -55,8 +64,18 @@ http.createServer(function (request, response) {
         return;
     }
 
+    //todo fazer função que carrega objetos que contêm os dados
+    //todo de utilizadores e ranking 
+    
+    console.log("=======Inicio Pedido=======");
+
+    console.log("--------------------");
+    console.log("Estado do objeto do inicio pedido");
+    console.log(UserDataObject);
+    console.log("--------------------");
+
+
     //Dividimos o que fazer com cada pedido consoante o /
-    console.log("=======Inicio Pedido=======")
     switch (pathname) {
         case '/register':
             console.log("Entrei no /register");
@@ -89,17 +108,74 @@ http.createServer(function (request, response) {
 
     // response.end("fim");
 
-}).listen(PORT);
+});
+
+//Pomos o Servidor Http à escuta mas antes disso , fazemos uma função 
+//anónima para carregar dados relevantes 
+Server.listen(PORT, () => {
+    console.log("Server Just Started");
+
+    //Inicializa objecto que guarda dados de utilizador 
+    GetUserData();
+
+    //todo fazer  objecto que guarda dados de ranking 
+});
 
 
-//Função que trata dos pedidos em /register
+//Objecto que guarda , em memória , o user.txt
+var UserDataObject = {};
+
+//carrega dados de utilizadores (quando inicia servidor) 
+function GetUserData() {
+    console.log("Entrei aqui");
+    if (fs.existsSync("user.txt")) {
+        console.log("Ficheiro já user.txt existe");
+        //carrega dados do ficheiro ...
+        fs.readFile('user.txt', 'utf8', (err, data) => {
+            if (err) {
+                console.error("Erro ao ler dados do arquivo:", err);
+                return;
+            }
+            if (data) {
+                try {
+                    UserDataObject = JSON.parse(data);
+                    // console.log(UserDataObject);
+                } catch (parseError) {
+                    console.error("Erro ao analisar dados do ficheiro user.txt : ", parseError);
+                    return;
+                }
+            }
+        });
+
+    }
+    else {
+        console.log("Ficheiro  user.txt não existe");
+        //criar ficheiro ...
+        //todo criar ficheiro  user.txt 
+        fs.writeFile('user.txt','',function(err){
+            if (err) throw err;
+            console.log("Criado ficheiro user.txt");
+        });
+    }
+}
+
+
+
+
+//Função que trata dos pedidos em /register 
+/* 
+TODO Adicionar o seguinte no método abaixo
+1) verificar se o ficheiro existe se não cria um ficheiro chamado user.txt (no primeiro registo)
+*/
 function register(request, response) {
 
     //obter dados do pedido (quando bloco de dados estiver disponivel)
     let requestData = '';
     request.on('data', chunk => {
         requestData += chunk.toString();
+        console.log("--------------------");
         console.log("Request Data = " + requestData);
+        console.log("--------------------");
     });
 
     //Quando a leitura terminar 
@@ -107,64 +183,47 @@ function register(request, response) {
         try {
             // Serialização dos dados no pedido
             const requestDataObj = JSON.parse(requestData);
+            //todo
+            //Modificar isto abaixo para ler e guardar dados através do objeto (usamos o objeto
+            //para evitar ter que estar a fazer a leitura de ficheiros).Em termos do ficheiro devemos
+            //ir guardando o proprio objeto stringificado lá   
 
-            // Ler os dados do arquivo
-            fs.readFile('user.txt', 'utf8', (err, data) => {
-                if (err) {
-                    console.error("Erro ao ler dados do arquivo:", err);
-                    response.writeHead(500, headers.plain);
-                    response.end('{"error": "Internal Server Error"}');
-                    return;
+            // Verificar se o utilizador já está registrado
+            if (UserDataObject[requestDataObj.nick]) {
+                // Verificar se a password está correta
+                if (UserDataObject[requestDataObj.nick] === requestDataObj.password) {
+                    // Sucesso: utilizador registado e password correta
+                    response.writeHead(200, headers.plain);
+                    response.end("{}");
+                    console.log("=======Fim Pedido=======");
+                } else {
+                    // password diferente
+                    response.writeHead(401, headers.plain);
+                    response.end('{"error": "User registered with a different password"}');
+                    console.log("=======Fim Pedido=======");
                 }
-
-                let userDatabase = {};
-
-                //Tentamos colocar os dados em user.txt (no objeto userDatabase)
-                if (data) {
-                    try {
-                        userDatabase = JSON.parse(data);
-                    } catch (parseError) {
-                        console.error("Erro ao analisar dados do arquivo JSON:", parseError);
+            } else {
+                // Utilizador não registrado(Adicionar)
+                UserDataObject[requestDataObj.nick] = requestDataObj.password;
+                console.log("--------------------");
+                console.log("Estado objeto após adicionamento");
+                console.log(UserDataObject);
+                console.log("--------------------");
+                // Escrever os dados atualizados no arquivo
+                fs.writeFile('user.txt', JSON.stringify(UserDataObject), 'utf8', (writeErr) => {
+                    if (writeErr) {
+                        console.error("Erro ao escrever dados no arquivo:", writeErr);
                         response.writeHead(500, headers.plain);
                         response.end('{"error": "Internal Server Error"}');
                         return;
                     }
-                }
 
-                // Verificar se o utilizador já está registrado
-                if (userDatabase[requestDataObj.nick]) {
-                    // Verificar se a password está correta
-                    if (userDatabase[requestDataObj.nick] === requestDataObj.password) {
-                        // Sucesso: utilizador registado e password correta
-                        response.writeHead(200, headers.plain);
-                        response.end("{}");
-                        console.log("=======Fim Pedido=======");
-                    } else {
-                        // password diferente
-                        response.writeHead(401, headers.plain);
-                        response.end('{"error": "User registered with a different password"}');
-                        console.log("=======Fim Pedido=======");
-                    }
-                } else {
-                    // Utilizador não registrado(Adicionar)
-                    userDatabase[requestDataObj.nick] = requestDataObj.password;
-
-                    // Escrever os dados atualizados no arquivo
-                    fs.writeFile('user.txt', JSON.stringify(userDatabase), 'utf8', (writeErr) => {
-                        if (writeErr) {
-                            console.error("Erro ao escrever dados no arquivo:", writeErr);
-                            response.writeHead(500, headers.plain);
-                            response.end('{"error": "Internal Server Error"}');
-                            return;
-                        }
-
-                        // Responder com sucesso
-                        response.writeHead(200, headers.plain);
-                        response.end("{}");
-                        console.log("=======Fim Pedido=======");
-                    });
-                }
-            });
+                    // Responder com sucesso
+                    response.writeHead(200, headers.plain);
+                    response.end("{}");
+                    console.log("=======Fim Pedido=======");
+                });
+            }
         } catch (error) {
             // Erro ao analisar os dados do pedido
             response.writeHead(400, headers.plain);
@@ -175,6 +234,12 @@ function register(request, response) {
 
 
 //Função que trata dos pedidos em /ranking
+/* 
+TODO Adicionar o seguinte no método abaixo
+1) verificar se o ficheiro existe se não cria (logo no inicio)
+2) resolver aquele problema de esperar até receber objeto vindo do 
+módulo rank.js
+*/
 function ranking(request, response) {
     //obter dados do pedido (quando bloco de dados estiver disponivel)
     let requestData = '';
@@ -234,14 +299,24 @@ function ranking(request, response) {
                 return;
             }
 
+            const tabela = new rank.Score();
+            //chamamos o construtor
+            tabela.Score(group, rows, columns);
+            console.log("Group = " + tabela.group);
+            console.log("Nr Linhas = " + tabela.rows);
+            console.log("Nr Colunas = " + tabela.columns);
+
+            //estou a ter problemas 
+            //pelo facto de execução não esperar que acaba 
+            //loadFromFile , indicar possiveis soluções que eu penso que funcionem 
+            // como uso de promessas
+            tabela.loadFromFile();
+            console.log(tabela.ranking);
+
+
             // Se todos os dados são válidos, pode retornar a resposta do ranking (por enquanto, uma resposta padrão sem dados)
             response.writeHead(200, headers.plain);
-            //todo Utilizador o modulo rank.js e criar uma função que vai buscar o ranking
-            //todo do grupo e retorna-o (ver como é que vamos guardar ranking com professor
-            //todo ,num ficheiro texto?)
 
-            const tabela = new rank.Score();
-            console.log(tabela);
 
 
             response.end('{ "ranking": [] }');
@@ -251,6 +326,8 @@ function ranking(request, response) {
             // Erro ao analisar os dados da solicitação
             response.writeHead(400, headers.plain);
             response.end('{"error": "Invalid request data"}');
+            console.log("=======Fim Pedido=======");
+
         }
     });
 }
