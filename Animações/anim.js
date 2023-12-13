343a07939aedcc8fb361111f42a985eae99d0439
RS181 <ruisantos.18181@gmail.com>
2023-12-13 09:51:15 +0000

    versão final da animação

--- Animações/anim.js
+++ Animações/anim.js
@@ -8,7 +8,7 @@ window.onload = function () {
     canvas = document.getElementById('myCanvas');
     context = canvas.getContext("2d");
     rotateAngle = 0;
-
+    canvas.style.visibility= "hidden";
     if (animateTheLoader) {
         loadingAnimation();
     }
@@ -16,49 +16,16 @@ window.onload = function () {
         loadingDrawing();
     }
 
+
 }
-function loadingDrawing() {
-    context.save();
 
-    context.translate(30, 30);
-    context.rotate(rotateAngle * Math.PI / 180);
-    context.translate(-30, -30);
 
-    context.beginPath();
-    context.strokeStyle = "black";
-    context.lineWidth = 2; // Ajuste a largura da linha
-    context.lineCap = "round";
+function StartAnimation(){
+    canvas.style.visibility = "visible" ;
 
-    context.fillStyle = "rgba(0,0,0,1)";
-    context.moveTo(30, 24);
-    context.lineTo(30, 8);
-    context.stroke();
-
-    context.strokeStyle = "rgba(0,0,0,0.8)";
-    context.moveTo(27, 27);
-    context.lineTo(18, 18);
-    context.stroke();
-
-    context.strokeStyle = "rgba(0,0,0,0.5)";
-    context.moveTo(24, 30);
-    context.lineTo(8, 30);
-    context.stroke();
-
-    context.strokeStyle = "rgba(0,0,0,0.35)";
-    context.moveTo(27, 33);
-    context.lineTo(18, 42);
-    context.stroke();
-
-    context.strokeStyle = "rgba(0,0,0,0.2)";
-    context.moveTo(30, 37);
-    context.lineTo(30, 48);
-    context.stroke();
-
-    context.closePath();
-
-    context.save();
-
-    context.restore();
+}
+function StopAnimation(){
+    canvas.style.visibility= "hidden";
 }
 
 function loadingDrawing() {
@@ -70,48 +37,34 @@ function loadingDrawing() {
     context.translate(centerX, centerY);
 
     context.rotate(rotateAngle * Math.PI / 180);
-    context.translate(-30, -30);
 
-    context.translate(30, 30);
-    context.rotate(rotateAngle * Math.PI / 180);
-    context.translate(-30, -30);
+    // Comprimento uniforme para todos os traços
+    var lineLength = 20;
 
-    context.beginPath();
-    context.strokeStyle = "black";
-    context.lineWidth = 2; // Ajuste a largura da linha
-    context.lineCap = "round";
+    // Array de cores
+    var colors = ["black", "#202020", "#606060", "#808080", "#A0A0A0", "#E0E0E0"];
 
-    context.fillStyle = "rgba(0,0,0,1)";
-    context.moveTo(30, 24);
-    context.lineTo(30, 8);
-    context.stroke();
+    // Desenha cada traço com uma cor diferente
+    for (var i = 0; i < 360; i += 60) {
+        context.beginPath();
+        context.strokeStyle = colors[i / 60];
+        context.lineWidth = 5; // Ajuste a largura da linha
+        context.lineCap = "round";
 
-    context.strokeStyle = "rgba(0,0,0,0.8)";
-    context.moveTo(27, 27);
-    context.lineTo(18, 18);
-    context.stroke();
+        // Move para o ponto inicial
+        context.moveTo(0, 0);
 
-    context.strokeStyle = "rgba(0,0,0,0.5)";
-    context.moveTo(24, 30);
-    context.lineTo(8, 30);
-    context.stroke();
+        // Desenha a linha
+        context.lineTo(0, -lineLength);
+        context.stroke();
 
-    context.strokeStyle = "rgba(0,0,0,0.35)";
-    context.moveTo(27, 33);
-    context.lineTo(18, 42);
-    context.stroke();
-
-    context.strokeStyle = "rgba(0,0,0,0.2)";
-    context.moveTo(30, 37);
-    context.lineTo(30, 48);
-    context.stroke();
-
-    context.closePath();
-
-    context.save();
+        // Retorna à posição inicial antes de rotacionar para o próximo traço
+        context.rotate(60 * Math.PI / 180);
+    }
 
     context.restore();
 }
+
 
 function loadingAnimation() {
     canvas.width = canvas.width; // redraw canvas for animation effect
